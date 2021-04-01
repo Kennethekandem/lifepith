c63050d64d99c7b48c8bb1732a5a56de1e7db3c5
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-02 00:18:45 +0100

    update

--- views/theory.php
+++ views/theory.php
@@ -56,16 +56,20 @@
                                                         </div>
                                                     </div>
                                                     <div class="elementor-row">
-                                                        <div class="elementor-text-editor elementor-column elementor-col-66 elementor-top-column elementor-clearfix" align="justify">
+                                                        <div class="elementor-text-editor elementor-column elementor-col-100 elementor-clearfix" align="justify">
                                                             <p class="theory-p">Our society bears the burden of birthing young leaders devoid of knowledge on how to thrive and create positive impact. A problem that stems from a mismanaged and non-inclusive educational system that we take no pride in. Gradually our educational system increases in number but not in corresponding quality and inclusiveness especially in low income communities.
                                                                 <br><br>
                                                                 <q class="big-qoute"><b>“The foundation of every state is the education of its youth.”</b> - Diogenes</q>
                                                                 <br><br>
                                                                 Sadly, the term “education” has been limited to grades and basic information in the four walls of a classroom. A significant number of youths step out of this, oblivious of life’s realities and skills, thereby adding to the pile of youths having nugatory effect on the society.
+                                                                <br><br>
+                                                                We understand that pointing fingers doesn't effect change. Therefore we have identified this problem and we’re making steps no matter how small to fix and sustain it.
+                                                                <br><br>
+                                                                With adequate trainings, mentorship, research materials, publications, innovative and developmental strategies; we can switch up the narrative of the every day youth thereby enhancing the sustainable development goal 4 (quality education) and ultimately increase goal 8 (decent work and economic growth) with measuring impact in years to come.
                                                             </p>
 <!--                                                            <p>“The foundation of every state is the education of its youth.” - Diogenes</p>-->
                                                         </div>
-                                                        <div class="elementor-column elementor-col-66 mb--top">
+                                                        <div class="elementor-column elementor-col-100 mb--top">
                                                             <img class="theory-center-image" src="assets/images/center.jpeg" alt="">
                                                         </div>
                                                     </div>
