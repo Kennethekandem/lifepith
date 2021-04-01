57191cd4214dc279777fb1bca41c930506bc9a8d
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-02 00:00:05 +0100

    update

--- views/theory.php
+++ views/theory.php
@@ -57,7 +57,14 @@
                                                     </div>
                                                     <div class="elementor-row">
                                                         <div class="elementor-text-editor elementor-column elementor-col-66 elementor-top-column elementor-clearfix" align="justify">
-                                                            <p class="theory-p">Our society bears the burden of birthing young leaders devoid of knowledge on how to thrive and create positive impact. A problem that stems from a mismanaged and non-inclusive educational system that we take no pride in. Gradually our educational system increases in number but not in corresponding quality and inclusiveness especially in low income communities.</p></div>
+                                                            <p class="theory-p">Our society bears the burden of birthing young leaders devoid of knowledge on how to thrive and create positive impact. A problem that stems from a mismanaged and non-inclusive educational system that we take no pride in. Gradually our educational system increases in number but not in corresponding quality and inclusiveness especially in low income communities.
+                                                                <br>
+                                                                <q><b>“The foundation of every state is the education of its youth.” - Diogenes</b></q>
+                                                                <br>
+                                                                Sadly, the term “education” has been limited to grades and basic information in the four walls of a classroom. A significant number of youths step out of this, oblivious of life’s realities and skills, thereby adding to the pile of youths having nugatory effect on the society.
+                                                            </p>
+<!--                                                            <p>“The foundation of every state is the education of its youth.” - Diogenes</p>-->
+                                                        </div>
                                                         <div class="elementor-column elementor-col-66 mb--top">
                                                             <img class="theory-center-image" src="assets/images/center.jpeg" alt="">
                                                         </div>
