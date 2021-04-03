aeb6e753494098adfa6480f3798440dcd81e1c7e
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-03 23:21:47 +0100

    update to authentication

--- views/admin.php
+++ views/admin.php
@@ -1,6 +1,6 @@
 <?php
 
-$pages = array("blogs");
+$pages = array("blogs", "login");
 
 if($routes[2] != '') {
     if(in_array($routes[2], $pages)) {
@@ -8,8 +8,37 @@ if($routes[2] != '') {
     }else {
         $page = 'blogs';
     }
+} else {
+    $page = 'blogs';
 }
 
-require("admin/partials/header.php");
-require("admin/" . $page . '.php');
-require("admin/partials/footer.php");
+if($page == 'login') {
+    if(isset($_SESSION['logged_admin'])) {
+        $page = 'blogs';
+    }
+    require("view/admin" . $page . ".php");
+} else {
+    if(!isset($_SESSION['logged_admin'])) {
+        $page = 'login';
+    } else {
+        $email = $_SESSION['logged_admin'];
+
+        $account = $db->query("SELECT * FROM admin WHERE email = :email", array('email' => $email), false);
+
+        if($account) {
+            $admin_id = $account['id'];
+            $admin_name = $account['name'];
+        }else {
+            unset($_SESSION['logged_admin']);
+            $page = 'login';
+        }
+    }
+}
+
+if($page == 'login') {
+    require("views/admin/auth/" . $page . ".php");
+}else {
+    require("views/admin/partials/header.php");
+    require("views/admin/" . $page . '.php');
+    require("views/admin/partials/footer.php");
+}
